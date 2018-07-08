5b7231e40d31560bde7dc15f7ba4d037822ccc16
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2018-07-08 10:53:27 +0100

    Finish CheckPasswordFragment

--- pwned.go
+++ pwned.go
@@ -2,20 +2,26 @@
 package pwned
 
 import (
+	"bytes"
+	"crypto/sha1"
 	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 const (
 	baseURL     = "https://api.pwnedpasswords.com"
 	passwordURL = baseURL + "/pwnedpassword/"
+	rangeURL    = baseURL + "/range/"
 )
 
-// CheckPassword calls the API and returns the number of times a password has occured
-func CheckPassword(password string) int {
+// CheckWholePassword calls the API and returns the number of times a password has occured
+func CheckWholePassword(password string) int {
 
 	//form string
 	URL := strings.Join([]string{passwordURL, password}, "")
@@ -40,4 +46,77 @@ func CheckPassword(password string) int {
 
 	return int(convertedInt)
 
+}
+
+func CheckPasswordFragment(password string) int {
+	//hash password
+	hashedPassword := hashPassword(password)
+
+	passPrefix, passSuffix := getPassFragments(hashedPassword)
+
+	fmt.Printf("%s\n", passPrefix)
+	fmt.Printf("length: %v\n", len(passPrefix))
+	//send to api
+	URL := strings.Join([]string{rangeURL, string(passPrefix)}, "")
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	//parse response
+	body, err := ioutil.ReadAll((resp.Body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(string(body))
+
+	lines := bytes.Split(body, []byte("\n"))
+	return findPassCount(lines, passSuffix)
+}
+
+func hashPassword(password string) []byte {
+	hasher := sha1.New()
+	hasher.Write([]byte(password))
+
+	return hasher.Sum(nil)
+}
+
+func getPassFragments(hashedPassword []byte) (passPrefix, passSuffix string) {
+	hexHash := hex.EncodeToString(hashedPassword)
+
+	prefix := hexHash[:5]
+	suffix := hexHash[5:]
+	return prefix, suffix
+}
+
+func parseLine(line []byte) ([]byte, int) {
+	s := bytes.Split(line, []byte(":"))
+	suffix := s[0]
+
+	//Remove \r line return character if present
+	c := bytes.Replace(s[1], []byte("\r"), []byte(""), 1)
+	count, err := strconv.Atoi(string(c))
+	if err != nil {
+		panic(err)
+	}
+	return suffix, count
+}
+
+func findPassCount(lines [][]byte, targetSuffix string) int {
+	var ret int
+	var found bool
+	for _, line := range lines {
+		suffix, count := parseLine(line)
+		if string(suffix) == strings.ToUpper(targetSuffix) {
+			ret = count
+			found = true
+			break
+		}
+	}
+	if !found {
+		panic("Not found" + targetSuffix)
+	}
+	return ret
 }
--- pwned_test.go
+++ pwned_test.go
@@ -1,19 +1,69 @@
 package pwned
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
 const checkMark = "\u2713"
 const ballotX = "\u2717"
+const examplePass = "password1"
 
 //TestCheckPassword validates that the API is working
-func TestCheckPassword(t *testing.T) {
-
-	examplePass := "Password"
-	//statusCode := 200
-
+func TestCheckWholePassword(t *testing.T) {
 	t.Logf("Making API call with example password: %v", examplePass)
-	response := CheckPassword(examplePass)
+	response := CheckWholePassword(examplePass)
 	t.Logf("Recieved %v, %v", response, checkMark)
+}
+
+func TestCheckPasswordFragment(t *testing.T) {
+	wantCount := 2310111
+	t.Logf("Marking API call with example password: %v", examplePass)
+	count := CheckPasswordFragment(examplePass)
+	t.Logf("Recieved %v", checkMark)
+	if count != wantCount {
+		t.Errorf("got %v, wanted %v", count, wantCount)
+	}
+}
+
+func TestHashPassword(t *testing.T) {
+	output := hashPassword(examplePass)
+	t.Logf("%x\n%v", string(output), output)
+}
+
+func TestParseLine(t *testing.T) {
+	inputString := []byte("00CBB2A6F377FACA93FA20D1A7D4D68EF9C:1\r")
+	suffix, count := parseLine(inputString)
+	fmt.Println(string(suffix), count)
+}
+
+func Test_getPassFragments(t *testing.T) {
+	type args struct {
+		hashedPassword []byte
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantPassPrefix string
+		wantPassSuffix string
+	}{
+		{
+			name:           "1",
+			args:           args{hashPassword(examplePass)},
+			wantPassPrefix: "e38ad",
+			wantPassSuffix: "214943daad1d64c102faec29de4afe9da3d",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotPassPrefix, gotPassSuffix := getPassFragments(tt.args.hashedPassword)
+			if !reflect.DeepEqual(gotPassPrefix, tt.wantPassPrefix) {
+				t.Errorf("getPassFragments() gotPassPrefix = %v, want %v", gotPassPrefix, tt.wantPassPrefix)
+			}
+			if !reflect.DeepEqual(gotPassSuffix, tt.wantPassSuffix) {
+				t.Errorf("getPassFragments() gotPassSuffix = %v, want %v", gotPassSuffix, tt.wantPassSuffix)
+			}
+		})
+	}
 }
